b1845cc4bf1cabd4c2851600dcf0c1d86ffb796d
yuanxiaofengzuishuai <1293365398@qq.com>
2017-05-22 23:07:27 +0800

    Revert "#4 #172 第四次作业"

--- AndroidLabs/app/src/main/java/edu/hzuapps/androidlabs/homeworks/net1414080903132/mapView.java
+++ AndroidLabs/app/src/main/java/edu/hzuapps/androidlabs/homeworks/net1414080903132/mapView.java
@@ -2,7 +2,6 @@ package edu.hzuapps.androidlabs.homework.net1414080903132;
 
 import android.content.Context;
 import android.content.res.TypedArray;
-import android.graphics.Paint;
 import android.util.AttributeSet;
 import android.view.View;
 
@@ -10,8 +9,7 @@ import edu.hzuapps.androidlabs.R;
 
 
 public class mapView extends View {
-    protected static int pointSize;// 地图点的大小。其实就是点的宽和高，大小一样
-    private final Paint mPaint = new Paint();//画笔，用来画蛇还有点
+    protected static int pointSize;
 
     public mapView(Context context, AttributeSet att, int def){
         super(context,att,def);
